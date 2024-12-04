68d15a0496461df576b1bb7e4f653d095d9d21dc
BlinH <BlinH@DESKTOP-3UTH7FO>
2024-12-03 21:16:09 -0800

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,33 @@
+﻿namespace BlakeHunt_Final
+{
+    public class Node
+    {
+        public Node next;
+        public Object data;
+    }
+    public class LinkedList
+    {
+        private Node head;
+
+        public void AllNodes()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+        public void AddNode(Object data)
+        {
+            Node add = new Node();
+            add.data = data;
+            Node current = head;
+            while (current != null)
+            {
+                current = current.next;
+            }
+            current.next = add;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BlakeHunt_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intro");
+            Console.Read();
+            Console.Clear();
+
+            //AddNode();
+
+            bool ween = true;
+            while (ween)
+            {
+                string input = Console.ReadLine();
+                //dont use "A" or "I" for controls.
+                if (input == "`")
+                {
+                    ween = false;
+                }
+            }
+        }
+    }
+}
